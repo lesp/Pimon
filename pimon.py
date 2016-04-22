726693c54be41b7bf8b70a285c289a4c02a8c971
Les Pounder <lespounder@gmail.com>
2016-04-22 10:33:03 +0000

    Updated and working to spec

--- pimon.py
+++ pimon.py
@@ -9,63 +9,70 @@ yellow = Button(27)
 blue = Button(22)
 
 
-LED_COUNT      = 360      # Number of LED pixels.
+LED_COUNT      = 150      # Number of LED pixels.
 LED_PIN        = 18      # GPIO pin connected to the pixels (must support PWM!).
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
-LED_BRIGHTNESS = 32     # Set to 0 for darkest and 255 for brightest
+LED_BRIGHTNESS = 128     # Set to 0 for darkest and 255 for brightest
 LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
 
 
 def neo(color,n):
     if color == "pink":
-        for i in range(90):
+        for i in range(150):
             strip.setPixelColor(i,Color(20,255,147))
         strip.show()
         sleep(n)
-        for i in range(90):
+        for i in range(150):
             strip.setPixelColor(i,Color(0,0,0))
+        strip.show()
     elif color == "green":
-        for i in range(90,180):
+        for i in range(150):
             strip.setPixelColor(i,Color(255,0,0))
         strip.show()
         sleep(n)
-        for i in range(90,180):
+        for i in range(150):
             strip.setPixelColor(i,Color(0,0,0))
-
+        strip.show()
     elif color == "blue":
-        for i in range(180,270):
+        for i in range(150):
             strip.setPixelColor(i,Color(0,0,255))
         strip.show()
         sleep(n)
-        for i in range(180,270):
+        for i in range(150):
             strip.setPixelColor(i,Color(0,0,0))
-
+        strip.show()
     elif color == "yellow":
-        for i in range(270,360):
+        for i in range(150):
             strip.setPixelColor(i,Color(255,255,0))
         strip.show()
         sleep(n)
-        for i in range(27,360):
+        for i in range(150):
             strip.setPixelColor(i,Color(0,0,0))
-
+        strip.show()
 def correct(n):
-    for i in range(3):
-        for i in range(360):
+    for i in range(9):
+        sleep(n)
+        for i in range(150):
             strip.setPixelColor(i,Color(255,0,0))
         strip.show()
         sleep(n)
-        for i in range(360):
+        for i in range(150):
             strip.setPixelColor(i,Color(0,0,0))
+        strip.show()
+        sleep(n)
 
 def wrong(n):
-    for i in range(3):
-        for i in range(360):
+    for i in range(9):
+        sleep(n)
+        for i in range(150):
             strip.setPixelColor(i,Color(0,255,0))
         strip.show()
         sleep(n)
-        for i in range(360):
-            strip.setPixelColor(i,Color(0,0,0)) 
+        for i in range(150):
+            strip.setPixelColor(i,Color(0,0,0))
+        strip.show()
+        sleep(n)
 
 
 
@@ -82,43 +89,45 @@ if __name__ == '__main__':
                     neo(light,0.1)
             random.shuffle(lights)
             print(lights)
-            sleep(1)
+            sleep(0.5)
             player = []
             for light in lights:
                 neo(light,0.3)    
-            for i in range(360):
+            for i in range(150):
                 strip.setPixelColor(i,Color(0,0,0))
                 strip.show()
             while len(player) <4:
-                sleep(0.4)
+                sleep(0.3)
                 if green.is_pressed:
                     player.append("green")
+                    neo("green",0.1)
                     print(player)
                 elif pink.is_pressed:
                     player.append("pink")
+                    neo("pink",0.1)
                     print(player)
                 elif yellow.is_pressed:
                     player.append("yellow")
+                    neo("yellow",0.1)
                     print(player)
                 elif blue.is_pressed:
                     player.append("blue")
+                    neo("blue",0.1)
                     print(player)        
             for choice in player:
                 neo(choice,0.5)
             if lights == player:
                 print("CORRECT")
-                correct(0.5)
-                for i in range(360):
+                correct(0.1)
+                for i in range(150):
                     strip.setPixelColor(i,Color(0,0,0))
                 strip.show()
             else:
                 print("WRONG ANSWER McFLY")
-                wrong(0.5)
-                for i in range(360):
+                wrong(0.1)
+                for i in range(150):
                     strip.setPixelColor(i,Color(0,0,0))
-                strip.show()  
-                    
-            
+                strip.show()            
     except KeyboardInterrupt:
         for i in range(360):
             strip.setPixelColor(i,Color(0,0,0))
